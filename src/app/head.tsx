a36c14ba1ac6fbddb42917c71512ec6b34a82fbb
Naveen <i.guptanaveen@gmail.com>
2025-08-20 22:24:28 +0530

    Add custom page title and meta description

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Naveen Gupta | Portfolio</title>
+      <meta name="description" content="Portfolio of Naveen Gupta - Aspiring Software Developer" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
